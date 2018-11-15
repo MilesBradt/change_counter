41950d6a04dad7f6dd08b562903c4ad69b114276
Miles Bradt <smbradtmichael@gmail.com>
2018-11-15 10:15:52 -0800

    have all forms of coins working

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,9 +1,12 @@
 class Coins
+
   def initialize()
+
 
     @coins = {
       1 => "penny",
-      5 => "nickle",
+      2 => "pennies",
+      5 => "nickel",
       10 => "dime",
       25 => "quarter"
     }
@@ -11,11 +14,43 @@ class Coins
   end
 
   def count_change(change)
+    output = ""
     coin_amount = 0
-    if change - 25 <= 25
-      coin_amount += 1
-      coin_amount.to_s + " " + @coins.fetch(25)
+    until change == 0 do
+      if change >= 25
+        coin_amount += 1
+        change -= 25
+        if coin_amount > 1
+        output = coin_amount.to_s + " " + @coins.fetch(25) + "s"
+        else
+        output = coin_amount.to_s + " " + @coins.fetch(25)
+        end
+      elsif change >= 10
+        coin_amount += 1
+        change -= 10
+        if coin_amount > 1
+        output = coin_amount.to_s + " " + @coins.fetch(10) + "s"
+        else
+        output = coin_amount.to_s + " " + @coins.fetch(10)
+        end
+      elsif change >= 5
+        coin_amount += 1
+        change -= 5
+        if coin_amount > 1
+        output = coin_amount.to_s + " " + @coins.fetch(5) + "s"
+        else
+        output = coin_amount.to_s + " " + @coins.fetch(5)
+        end
+      elsif change >= 1
+        coin_amount += 1
+        change -= 1
+        if coin_amount > 1
+        output = coin_amount.to_s + " " + @coins.fetch(2)
+        else
+        output = coin_amount.to_s + " " + @coins.fetch(1)
+        end
+      end
     end
+    return output
   end
-
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -4,9 +4,29 @@ require ('pry')
 
 
 describe("#count_change") do
-  it("Take 50 and give 25 + 25") do
+  it("Take 25 and give '1 quarter'") do
     user_coins = Coins.new()
     expect(user_coins.count_change(25)).to(eq("1 quarter"))
+  end
+
+  it("Take 50 and give '2 quarters'") do
+    user_coins = Coins.new()
+    expect(user_coins.count_change(50)).to(eq("2 quarters"))
+  end
+
+  it("Take 10 and give '1 dime'") do
+    user_coins = Coins.new()
+    expect(user_coins.count_change(10)).to(eq("1 dime"))
+  end
+
+  it("Take 5 and give '1 nickel'") do
+    user_coins = Coins.new()
+    expect(user_coins.count_change(5)).to(eq("1 nickel"))
+  end
+
+  it("Take 1 and give '1 penny'") do
+    user_coins = Coins.new()
+    expect(user_coins.count_change(3)).to(eq("3 pennies"))
   end
 
 end
