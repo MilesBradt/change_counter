2dd3a32b0eb915ef7afa8818a1431299447515c1
Miles Bradt <smbradtmichael@gmail.com>
2018-11-15 11:36:20 -0800

    take change argument and output best amount

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,16 +1,11 @@
 class Coins
-
   def initialize()
-
-
     @coins = {
-      1 => "penny",
-      2 => "pennies",
+      1 => "penn",
       5 => "nickel",
       10 => "dime",
       25 => "quarter"
     }
-
   end
 
   def count_change(change)
@@ -21,42 +16,51 @@ class Coins
     penny_amount = 0
     until change == 0 do
       if change >= 25
-        binding.pry
         quarter_amount += 1
         change -= 25
-        if quarter_amount > 1
+        if (quarter_amount > 1) & (change != 0)
+        output = quarter_amount.to_s + " " + @coins.fetch(25) + "s, "
+        elsif change != 0
+        output = quarter_amount.to_s + " " + @coins.fetch(25) + ", "
+        elsif (quarter_amount > 1) & (change == 0)
         output = quarter_amount.to_s + " " + @coins.fetch(25) + "s"
-        else
+        elsif change == 0
         output = quarter_amount.to_s + " " + @coins.fetch(25)
         end
       elsif (change >= 10) & (change < 25)
-        binding.pry
         dime_amount += 1
         change -= 10
-        if dime_amount > 1
+        if (dime_amount > 1) & (change != 0)
+        output = output + dime_amount.to_s + " " + @coins.fetch(10) + "s, "
+        elsif change != 0
+        output = output + dime_amount.to_s + " " + @coins.fetch(10) + ", "
+        elsif (dime_amount > 1) & (change == 0)
         output = output + dime_amount.to_s + " " + @coins.fetch(10) + "s"
-        else
+        elsif (change == 0)
         output = output + dime_amount.to_s + " " + @coins.fetch(10)
         end
       elsif (change >= 5) & (change < 10)
-        binding.pry
         nickel_amount += 1
         change -= 5
-        if nickel_amount > 1
+        if (nickel_amount > 1) & (change != 0)
+        output = output + nickel_amount.to_s + " " + @coins.fetch(5) + "s, "
+        elsif change != 0
+        output = output + nickel_amount.to_s + " " + @coins.fetch(5) + ", "
+      elsif (nickel_amount > 1) & (change == 0)
         output = output + nickel_amount.to_s + " " + @coins.fetch(5) + "s"
-        else
+        elsif (change == 0)
         output = output + nickel_amount.to_s + " " + @coins.fetch(5)
         end
       elsif (change >= 1) & (change < 5)
         penny_amount += 1
         change -= 1
-        if penny_amount > 1
-        output = output + penny_amount.to_s + " " + @coins.fetch(2)
-        else
-        output = output + penny_amount.to_s + " " + @coins.fetch(1)
+        if (penny_amount > 1) & (change == 0)
+        output = output + penny_amount.to_s + " " + @coins.fetch(1) + "ies"
+        elsif (change == 0)
+        output = output + penny_amount.to_s + " " + @coins.fetch(1) + "y"
         end
       end
     end
-    return output
+    puts output
   end
 end
